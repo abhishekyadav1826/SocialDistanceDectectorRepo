acb588ddb8a5c52d997dedfead2ebc7e4402cf1f
Abhishek Yadav <abhishekyadav1826@gmail.com>
2021-07-21 23:10:32 +0530

    initial commit

---
+++ ObjectDetectionFile/social_distancing_config.py
@@ -0,0 +1,7 @@
+MODEL_PATH = "yolo-coco"
+MIN_CONF = 0.3
+NMS_THRESH = 0.3
+# boolean indicating if NVIDIA CUDA GPU should be used
+USE_GPU = False
+# define the minimum safe distance (in pixels) that two people can be
+MIN_DISTANCE = 50
